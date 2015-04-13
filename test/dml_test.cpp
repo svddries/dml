2e5e27a2226ab06687a3e35ce651a9644b130ce8
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-04-13 09:51:45 +0200

    Detects more edges

--- test/dml_test.cpp
+++ test/dml_test.cpp
@@ -2,6 +2,73 @@
 #include "rgbd/View.h"
 #include <opencv2/highgui/highgui.hpp>
 
+float bla = 0.02;
+float max_range = 5;
+
+int findEdgeVertical(int x, int y_start, int y_end, const rgbd::View view, cv::Mat& canvas, std::string indent = "")
+{
+    if (x == 320)
+        std::cout << indent << y_start << " - " << y_end << std::endl;
+
+//    if (indent.size() > 0)
+//        cv::line(canvas, cv::Point(x, y_start), cv::Point(x, y_end), cv::Scalar(255, 0, 0));
+//    else
+//        cv::line(canvas, cv::Point(x, y_start + 1), cv::Point(x, y_end - 1), cv::Scalar(0, 0, 255));
+
+    geo::Vector3 p1_3d, p2_3d;
+    if (!view.getPoint3D(x, y_start, p1_3d) || !view.getPoint3D(x, y_end, p2_3d))
+        return -1;
+
+    geo::Vec2 p1(p1_3d.y, -p1_3d.z);
+    geo::Vec2 p2(p2_3d.y, -p2_3d.z);
+    geo::Vec2 n = (p2 - p1).normalized();
+
+    int y_edge = -1;
+    float max_dist_sq = 0;
+    for(int y2 = y_start; y2 <= y_end; ++y2)
+    {
+        geo::Vector3 p_3d;
+        if (!view.getPoint3D(x, y2, p_3d))
+            continue;
+
+        geo::Vec2 p(p_3d.y, -p_3d.z);
+
+        // Calculate distance of p to line (p1, p2)
+        geo::Vec2 p1_p = p - p1;
+
+        float dist_sq = (p1_p - (p1_p.dot(n) * n)).length2();
+
+        if (dist_sq > max_dist_sq)
+        {
+            max_dist_sq = dist_sq;
+            y_edge = y2;
+        }
+    }
+
+    if (y_edge < 0)
+        return -1;
+
+    float th = view.getDepth(x, y_edge) * bla;
+    if (max_dist_sq < (th * th))
+        return -1;
+
+    // Visualize
+    if (indent.size() > 0)
+        canvas.at<cv::Vec3b>(y_edge, x) = cv::Vec3b(0, 0, 255);
+    else
+        canvas.at<cv::Vec3b>(y_edge, x) = cv::Vec3b(0, 255, 0);
+
+
+
+    // Recursively find edges
+    findEdgeVertical(x, y_start, y_edge, view, canvas, indent + "    ");
+//    y_edge = findEdgeVertical(x, y_edge, y_end, view, canvas);
+
+    return y_edge;
+}
+
+// ----------------------------------------------------------------------------------------------------
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "dml_test");
@@ -9,8 +76,6 @@ int main(int argc, char **argv)
 
     rgbd::Client client;
     client.intialize("/amigo/top_kinect/rgbd");
-
-    float bla = 0.02;
 
     ros::Rate r(30);
     while (ros::ok())
@@ -132,7 +197,7 @@ int main(int argc, char **argv)
             for(; y < depth.rows; ++y)
             {
                 float d = depth.at<float>(y, x);
-                if (d == d && d > 0)
+                if (d == d && d > 0 && d < max_range)
                 {
                     y_start = y;
                     d_min = d;
@@ -147,7 +212,7 @@ int main(int argc, char **argv)
                 int y_end = y;
 
                 float d = depth.at<float>(y, x);
-                if (d != d || d == 0)
+                if (d != d || d == 0 || d > max_range)
                     continue;
 
                 int y_edge = -1;
@@ -161,56 +226,20 @@ int main(int argc, char **argv)
                     d_min = std::min(d, d_min);
                     d_max = std::max(d, d_max);
 
-                    if (d_max - d_min > d * bla && y_end - y_start >= 30) // TODO: magic number
+//                    if (d_max - d_min > d * bla && y_end - y_start >= 30) // TODO: magic number
+                    if (y_end - y_start >= 30) // TODO: magic number
                         check_edge = true;
                 }
 
                 if (check_edge)
                 {
-                    geo::Vector3 p1_3d, p2_3d;
-                    if (view.getPoint3D(x, y_start, p1_3d) && view.getPoint3D(x, y_end, p2_3d))
+                    y_edge = findEdgeVertical(x, y_start, y_end, view, canvas);
+
+                    if (y_edge == -1)
                     {
-                        geo::Vec2 p1(p1_3d.y, -p1_3d.z);
-                        geo::Vec2 p2(p2_3d.y, -p2_3d.z);
-                        geo::Vec2 n = (p2 - p1).normalized();
-
-                        float max_dist_sq = 0;
-                        for(int y2 = y_start; y2 <= y_end; ++y2)
-                        {
-                            geo::Vector3 p_3d;
-                            if (view.getPoint3D(x, y2, p_3d))
-                            {
-                                geo::Vec2 p(p_3d.y, -p_3d.z);
-
-                                // Calculate distance of p to line (p1, p2)
-                                geo::Vec2 p1_p = p - p1;
-
-                                float dist_sq = (p1_p - (p1_p.dot(n) * n)).length2();
-
-                                if (dist_sq > max_dist_sq)
-                                {
-                                    max_dist_sq = dist_sq;
-                                    y_edge = y2;
-                                }
-                            }
-                        }
-
-//                        canvas.at<cv::Vec3b>(y_start + 1, x) = cv::Vec3b(255, 0, 0);
-//                        canvas.at<cv::Vec3b>(y_end - 1, x) = cv::Vec3b(0, 0, 255);
-
-                        if (y_edge >= 0)
-                        {
-                            float th = depth.at<float>(y_edge, x) * bla;
-                            if (max_dist_sq < (th * th))
-                                y_edge = -1;
-                        }
-
-                        if (y_edge == -1)
-                        {
-                            y_start = y;
-                            d_min = depth.at<float>(y, x);
-                            d_max = d_min;
-                        }
+                        y_start = (y_start + y_end) / 2;
+                        d_min = depth.at<float>(y_start, x);
+                        d_max = d_min;
                     }
                 }
 
@@ -222,8 +251,8 @@ int main(int argc, char **argv)
                         y = y_edge;
                     }
 
-                    y_start = y;
-                    d_min = depth.at<float>(y, x);
+                    y_start = (y_start + y_end) / 2;
+                    d_min = depth.at<float>(y_start, x);
                     d_max = d_min;
                 }
 
