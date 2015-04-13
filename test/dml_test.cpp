bbccac63bdcd84503494202ab1525b0957481af3
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-04-13 10:34:43 +0200

    Simplified and cleaned up algorithm

--- test/dml_test.cpp
+++ test/dml_test.cpp
@@ -5,7 +5,64 @@
 float bla = 0.02;
 float max_range = 5;
 
-int findEdgeVertical(int x, int y_start, int y_end, const rgbd::View view, cv::Mat& canvas, std::string indent = "")
+// ----------------------------------------------------------------------------------------------------
+
+int findEdgeHorizontal(int y, int x_start, int x_end, const rgbd::View view, cv::Mat& canvas)
+{
+    geo::Vector3 p1_3d, p2_3d;
+    if (!view.getPoint3D(x_start, y, p1_3d) || !view.getPoint3D(x_end, y, p2_3d))
+        return -1;
+
+    geo::Vec2 p1(p1_3d.x, -p1_3d.z);
+    geo::Vec2 p2(p2_3d.x, -p2_3d.z);
+    geo::Vec2 n = (p2 - p1).normalized();
+
+    int x_edge = -1;
+    float max_dist_sq = 0;
+    for(int x2 = x_start; x2 <= x_end; ++x2)
+    {
+        geo::Vector3 p_3d;
+        if (!view.getPoint3D(x2, y, p_3d))
+            continue;
+
+        geo::Vec2 p(p_3d.x, -p_3d.z);
+
+        // Calculate distance of p to line (p1, p2)
+        geo::Vec2 p1_p = p - p1;
+
+        float dist_sq = (p1_p - (p1_p.dot(n) * n)).length2();
+
+        if (dist_sq > max_dist_sq)
+        {
+            max_dist_sq = dist_sq;
+            x_edge = x2;
+        }
+    }
+
+    if (x_edge < 0)
+        return -1;
+
+    float d = view.getDepth(x_edge, y);
+    if (d > max_range)
+        return -1;
+
+    float th = d * bla;
+    if (max_dist_sq < (th * th))
+        return -1;
+
+    // Visualize
+    canvas.at<cv::Vec3b>(y, x_edge) = cv::Vec3b(0, 0, 255);
+
+    // Recursively find edges
+    findEdgeHorizontal(y, x_start, x_edge, view, canvas);
+
+    return x_edge;
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+
+int findEdgeVertical(int x, int y_start, int y_end, const rgbd::View view, cv::Mat& canvas)
 {
     geo::Vector3 p1_3d, p2_3d;
     if (!view.getPoint3D(x, y_start, p1_3d) || !view.getPoint3D(x, y_end, p2_3d))
@@ -40,21 +97,19 @@ int findEdgeVertical(int x, int y_start, int y_end, const rgbd::View view, cv::M
     if (y_edge < 0)
         return -1;
 
-    float th = view.getDepth(x, y_edge) * bla;
+    float d = view.getDepth(x, y_edge);
+    if (d > max_range)
+        return -1;
+
+    float th = d * bla;
     if (max_dist_sq < (th * th))
         return -1;
 
     // Visualize
-    if (indent.size() > 0)
-        canvas.at<cv::Vec3b>(y_edge, x) = cv::Vec3b(0, 0, 255);
-    else
-        canvas.at<cv::Vec3b>(y_edge, x) = cv::Vec3b(0, 255, 0);
-
-
+    canvas.at<cv::Vec3b>(y_edge, x) = cv::Vec3b(0, 255, 0);
 
     // Recursively find edges
-    findEdgeVertical(x, y_start, y_edge, view, canvas, indent + "    ");
-//    y_edge = findEdgeVertical(x, y_edge, y_end, view, canvas);
+    findEdgeVertical(x, y_start, y_edge, view, canvas);
 
     return y_edge;
 }
@@ -97,84 +152,46 @@ int main(int argc, char **argv)
                 canvas.at<cv::Vec3b>(i) = cv::Vec3b(100, 0, 0);
         }
 
-
-//        for(int x = 0; x < depth.cols; ++x)
-//        {
-//            for(int y = 1; y < depth.rows + 1; ++y)
-//            {
-//                float& d1 = depth.at<float>(y - 1, x);
-//                float& d2 = depth.at<float>(y, x);
-//                float& d3 = depth.at<float>(y + 1, x);
-
-//                if (d1 != d1 || d1 == 0 || d2 != d2 || d2 == 0 || d3 != d3 || d3 == 0)
-//                    continue;
-
-//                float diff1 = d2 - d1;
-//                float diff2 = d2 - d3;
-
-//                if (diff1 * diff2 > 0) // && std::abs(diff1) < 0.1 && std::abs(diff2) < 0.1)
-//                    d2 = d1;
-//            }
-//        }
-
-
-
-
         // - - - - - - - - - - - - HORIZONTAL - - - - - - - - - - - -
 
-//        for(int y = 0; y < depth.rows; ++y)
-//        {
-//            step = 50;
-//            for(int x = 0; x < depth.cols - step; x += step / 2)
-//            {
-//                float d = depth.at<float>(y, x);
-//                if (d != d || d == 0)
-//                    continue;
+        for(int y = 0; y < depth.rows; ++y)
+        {
+            int x = 0;
+            int x_start;
 
-//                step = 50 / d;
-//                int x_end = std::min(depth.cols, x + step);
+            // Find first valid value
+            for(; x < depth.cols; ++x)
+            {
+                float d = depth.at<float>(y, x);
+                if (d == d && d > 0)
+                {
+                    x_start = x;
+                    break;
+                }
+            }
 
-//                geo::Vector3 p1_3d, p2_3d;
-//                if (!view.getPoint3D(x, y, p1_3d) || !view.getPoint3D(x_end, y, p2_3d))
-//                    continue;
+            for(; x < depth.cols; ++x)
+            {
+                int x_end = x;
 
-//                geo::Vec2 p1(p1_3d.x, -p1_3d.z);
-//                geo::Vec2 p2(p2_3d.x, -p2_3d.z);
-//                geo::Vec2 n = (p2 - p1).normalized();
+                float d = depth.at<float>(y, x);
+                if (d != d || d == 0)
+                    continue;
 
-//                int x2_best = -1;
-//                float max_dist_sq = 0;
+                if (x_end - x_start >= 30) // TODO: magic number
+                {
+                    int x_edge = findEdgeHorizontal(y, x_start, x_end, view, canvas);
 
-//                for(int x2 = x; x2 <= x_end; ++x2)
-//                {
-//                    geo::Vector3 p_3d;
-//                    if (!view.getPoint3D(x2, y, p_3d))
-//                        continue;
-
-//                    geo::Vec2 p(p_3d.x, -p_3d.z);
-
-//                    // Calculate distance of p to line (p1, p2)
-//                    geo::Vec2 p1_p = p - p1;
-
-//                    float dist_sq = (p1_p - (p1_p.dot(n) * n)).length2();
-
-//                    if (dist_sq > max_dist_sq)
-//                    {
-//                        max_dist_sq = dist_sq;
-//                        x2_best = x2;
-//                    }
-//                }
-
-//                if (x2_best >= 0)
-//                {
-//                    float th = depth.at<float>(y, x2_best) * bla;
-//                    if (max_dist_sq > (th * th))
-//                        canvas.at<cv::Vec3b>(y, x2_best) = cv::Vec3b(0, 0, 255);
-
-//                    x = x2_best + 1;
-//                }
-//            }
-//        }
+                    if (x_edge >= 0)
+                    {
+                        x = x_edge;
+                        x_start = (x_start + x_end) / 2;
+                    }
+                    else
+                        x_start = (x_start + x_end) / 2;
+                }
+            }
+        }
 
         // - - - - - - - - - - - - VERTICAL - - - - - - - - - - - -
 
@@ -182,16 +199,14 @@ int main(int argc, char **argv)
         {
             int y = 0;
             int y_start;
-            float d_last;
 
             // Find first valid value
             for(; y < depth.rows; ++y)
             {
                 float d = depth.at<float>(y, x);
-                if (d == d && d > 0 && d < max_range)
+                if (d == d && d > 0)
                 {
                     y_start = y;
-                    d_last = d;
                     break;
                 }
             }
@@ -201,36 +216,21 @@ int main(int argc, char **argv)
                 int y_end = y;
 
                 float d = depth.at<float>(y, x);
-                if (d != d || d == 0 || d > max_range)
+                if (d != d || d == 0)
                     continue;
 
-                int y_edge = -1;
-                if (std::abs(d - d_last) > 0.1 * d)   // TODO: magic number
+                if (y_end - y_start >= 30) // TODO: magic number
                 {
-                    y_edge = y;
-                }
-                else
-                {
-                    if (y_end - y_start >= 30) // TODO: magic number
+                    int y_edge = findEdgeVertical(x, y_start, y_end, view, canvas);
+
+                    if (y_edge >= 0)
                     {
-                        y_edge = findEdgeVertical(x, y_start, y_end, view, canvas);
-
-                        if (y_edge == -1)
-                        {
-                            y_start = (y_start + y_end) / 2;
-                        }
+                        y = y_edge;
+                        y_start = (y_start + y_end) / 2;
                     }
+                    else
+                        y_start = (y_start + y_end) / 2;
                 }
-
-                if (y_edge >= 0)
-                {
-                    canvas.at<cv::Vec3b>(y_edge, x) = cv::Vec3b(0, 255, 0);
-                    y = y_edge;
-
-                    y_start = (y_start + y_end) / 2;
-                }
-
-                d_last = depth.at<float>(y, x);
             }
         }
 
